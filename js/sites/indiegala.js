e8a6943d6d708b7cde30b352e6c091d446587a68
Wes Rich <elopdat@gmail.com>
2015-01-29 17:10:10 -0500

    Clean up some console logs.

--- js/sites/indiegala.js
+++ js/sites/indiegala.js
@@ -10,11 +10,9 @@ function IndieGala() {
   }
 
   this.handleGames = function(user_data) {
-    console.log( user_data );
     $('.games-box .b_game').each(function(index, game_image) {
       var game = _self.game_list[index],
           owned = user_data.games[game];
-      // console.log( game_image, _self.banner(owned) );
       $(game_image).prepend( _self.banner(owned) );
     });
   }
