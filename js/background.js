a191152d292e78bf0002092153063ee5c3d9dcf9
Wes Rich <elopdat@gmail.com>
2015-01-29 19:01:07 -0500

    Add Google Analytics, to help monitor usage so I can optimize most often used functionality.

--- js/background.js
+++ js/background.js
@@ -2,13 +2,23 @@ var api_domain = 'hb-steam-checker.herokuapp.com',
     api_url = 'http://'+api_domain,
     steam_id = '';
 
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-54095934-2']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 chrome.runtime.onMessage.addListener(function(request, sender) {
   switch (request.action) {
     case 'steam_sign_in':
-      check_steam_auth(sender.tab.id);
+      check_steam_auth(sender.tab);
       break;
     case 'fetch_games':
-      fetch_games(request.data, sender.tab.id);
+      fetch_games(request.data, sender.tab);
       break;
     default:
       console.warn( 'Unhandled Request' );
@@ -22,18 +32,19 @@ function check_steam_auth(calling_tab) {
     chrome.storage.sync.get('steam_id', function(result) {
       if (result.hasOwnProperty('steam_id')) {
         steam_id = result.steam_id;
-        chrome.tabs.sendMessage(calling_tab, {action: 'authenticated'});
+        chrome.tabs.sendMessage(calling_tab.id, {action: 'authenticated'});
       } else {
         steam_sign_in(calling_tab);
       }
     });
   } else {
-    chrome.tabs.sendMessage(calling_tab, {action: 'authenticated'});
+    chrome.tabs.sendMessage(calling_tab.id, {action: 'authenticated'});
   }
 }
 
 function steam_sign_in(calling_tab) {
   var url, auth_timer;
+  _gaq.push(['_trackEvent', 'Chrome Extension', 'steam_sign_in', calling_tab.title]);
   chrome.tabs.create({
     url: api_url+'/auth/steam',
     active: false
@@ -53,7 +64,8 @@ function steam_sign_in(calling_tab) {
               clearInterval( auth_timer );
               steam_id = url[url.length-1];
               chrome.storage.sync.set({'steam_id': steam_id});
-              chrome.tabs.sendMessage(calling_tab, {action: 'authenticated'});
+              _gaq.push(['_trackEvent', 'Chrome Extension', 'authenticated', calling_tab.title]);
+              chrome.tabs.sendMessage(calling_tab.id, {action: 'authenticated'});
               chrome.windows.remove(win.id);
             }
           } catch (ex) {
@@ -68,11 +80,12 @@ function steam_sign_in(calling_tab) {
 
 function fetch_games(game_list, calling_tab) {
   if (steam_id == '') { return false; }
+  _gaq.push(['_trackEvent', 'Chrome Extension', 'fetch_games', calling_tab.title]);
   var fetchOwnedSteamGames = $.ajax({
     url: api_url+"/users/"+steam_id+"/games",
     type: "GET",
     data: { "list": game_list.join(',') }
   }).done(function(data) {
-    chrome.tabs.sendMessage(calling_tab, {action: 'game_response', data: data})
+    chrome.tabs.sendMessage(calling_tab.id, {action: 'game_response', data: data})
   });
 }
